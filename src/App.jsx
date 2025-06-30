62dc3e3375b7124441ebf68d476d509a6dbfa0b2
EnestoHrz <xernestohrz@gmail.com>
2025-06-30 16:21:09 -0400

    API POKEMON

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import PokemonFetcher from './PokemonFetcher.jsx';
+
+function App(){
+  return(
+  <>
+    <h1> !Conoce a tus Pokemons!  </h1>
+    <PokemonFetcher/>
+  </>
+  );
+}
+export default App;
